bb416d8cee549d79110473d7188c0d97f465ef09
Deiby Toralva <dtoralva@moventicorp.com>
2024-10-18 02:39:56 -0500

    Initial Commit

---
+++ ApuestaTotal/ApuestaTotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApuestaTotalApp.swift
+//  ApuestaTotal
+//
+//  Created by Deiby Toralva on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ApuestaTotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
